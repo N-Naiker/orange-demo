cb6e918455ca321eee38ae33831cb9ccb5c868ea
N-Naiker <88787517+N-Naiker@users.noreply.github.com>
2022-09-13 15:57:07 +0200

    Update OWDataSamplerA.py

--- orangedemo/OWDataSamplerA.py
+++ orangedemo/OWDataSamplerA.py
@@ -28,3 +28,18 @@ class OWDataSamplerA(widget.OWWidget):
         self.infoa = gui.widgetLabel(
             box, "No data on input yet, waiting to get something.")
         self.infob = gui.widgetLabel(box, '')
+        
+        @Inputs.data
+        def set_data(self, dataset):
+        if dataset is not None:
+            self.infoa.setText("%d instances in input data set" % len(dataset))
+            indices = numpy.random.permutation(len(dataset))
+            indices = indices[:int(numpy.ceil(len(dataset) * 0.1))]
+            sample = dataset[indices]
+            self.infob.setText("%d sampled instances" % len(sample))
+            self.Outputs.sample.send(sample)
+        else:
+            self.infoa.setText(
+                "No data on input yet, waiting to get something.")
+            self.infob.setText('')
+            self.Outputs.sample.send(None)
